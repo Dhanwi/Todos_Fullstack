558e98abfdef734658b9b5f809a8ef936f7af17e
Priya Kumari <dbgpriyakumari@gmail.com>
2026-03-01 16:41:56 +0530

    frontend deployed url attached

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ const cors = require("cors");
 app.use(cors({
   origin: [
     "http://localhost:5173",
+    "https://todofrontend-xi-bay.vercel.app/"
   ]
 }));
 /************  part1 ****************/
