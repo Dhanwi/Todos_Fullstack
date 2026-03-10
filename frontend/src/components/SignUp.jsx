f60ec2f1b40855473b51cc848498c28315d1cddf
Priya Kumari <dbgpriyakumari@gmail.com>
2026-03-10 23:19:52 +0530

    signup backend logic attached in frontend via axios

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,24 +1,51 @@
-import React from 'react'
+import axios from 'axios';
+import React, { use } from 'react'
 import { useState } from 'react'
 
 export default function SignUP() {
+    const [credential, setCredential] = useState([]);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [username, setUsername] = useState('');
 
+    const handleUserName =(e)=>{
+        setUsername(e.target.value)
+    }
+    const handlePassword =(e)=>{
+        setPassword(e.target.value)
+    }
+    const handleEmail =(e)=>{
+        setEmail(e.target.value)
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        const payload = {email, password, userName: username}
+        axios.post(`${import.meta.env.VITE_API_URL}/todo/signUp`, payload)
+        .then((res)=>{
+            setCredential(res.data)
+        })
+        .finally(()=>{
+            setEmail('');
+            setPassword('');
+            setUsername('');
+        })
+        .catch(console.error);
+    }
     
   return (
     <div>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>Username:
-                <input value={username}/>
+                <input value={username} onChange={handleUserName}/>
             </label>
             <label>Email:
-                <input value={email}/>
+                <input value={email} onChange={handleEmail}/>
             </label>
             <label>Password:
-                <input value={password}/>
+                <input value={password} onChange={handlePassword}/>
             </label>
+            <input type='submit' className=' border-2 border-amber-900'/>
         </form>
     </div>
   )
