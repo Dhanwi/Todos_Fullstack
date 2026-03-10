677f8aa66d1c7cc24eb74fc9301f754b689c4221
Priya Kumari <dbgpriyakumari@gmail.com>
2026-03-10 23:37:14 +0530

    signIn backend logic attached in frontend via axios

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import SignUP from './SignUP'
+import SignIn from './SignIn'
 
 export default function Home() {
+  
   return (
-    <div>Home
-    <SignUP/>
+    <div>
+      
+      <div>signIn</div>
+      <SignIn />
+      <div>signUp</div>
+      <SignUP/>
     </div>
   )
 }
--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -1,20 +1,40 @@
-import React from 'react'
+import React, { use, useState } from 'react'
+import axios from 'axios';
+
 
 export default function SignIn() {
-  const [email, setEmail] = useState('');
+      const [credential, setCredential] = useState([])
+      const [email, setEmail] = useState('');
       const [password, setPassword] = useState('');
-      const [username, setUsername] = useState('');
-  
+      
+
+      const handleEmail=(e)=>{
+        setEmail(e.target.value)
+      }
+      const handlePassword = (e)=>{
+        setPassword(e.target.value)
+      }
+
+      const handleSubmit=(e)=>{
+        e.preventDefault();
+        const payload = {email, password}
+        axios.post(`${import.meta.env.VITE_API_URL}/todo/signIn`, payload)
+        .then((res)=>{
+            setCredential(res.data)
+        })
+        .catch(console.error)
+      }
       
     return (
       <div>
-          <form>
+          <form onSubmit={handleSubmit}>
               <label>Email:
-                  <input value={email}/>
+                  <input value={email} onChange={handleEmail}/>
               </label>
               <label>Password:
-                  <input value={password}/>
+                  <input value={password} onChange={handlePassword}/>
               </label>
+              <input type='submit' className=' border-2 border-amber-900' />
           </form>
       </div>
     )
